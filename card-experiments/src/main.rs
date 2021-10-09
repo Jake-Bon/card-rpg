bf6ff3d5c60d673b0489f90043349811a8fe6b29
jb <jacob.c.bonhomme@gmail.com>
2021-10-08 23:42:48 -0400

    Player functionality + error

--- card-experiments/src/main.rs
+++ card-experiments/src/main.rs
@@ -1,7 +1,26 @@
 //extern crate card_experiments;
-mod card;
+mod game_structs;
+
+use std::collections::VecDeque;
+use game_structs::CardType;
+use game_structs::Card;
+use game_structs::Player;
 //For card testing
 fn main(){
-    let aCard = card::Card::new("Test","This a test",3,card::CardType::Heal,3);
-    print!("{}",aCard.toString());
+    let mut aCard = Card::new("Test","This a test",3,CardType::Heal,3);
+    print!("{}\n",aCard.toString());
+
+    let mut aPlayer = Player::new(20,20,10,10,7);
+    print!("{}\n",aPlayer.toString());
+
+    aPlayer.addCardToDeck(aCard);
+
+
+    //error handling here
+
+    aPlayer.addCardToHand(aPlayer.drawCard().unwrap());
+
+    print!("{}\n",aPlayer.selectHand(0).toString());
+
+
 }
