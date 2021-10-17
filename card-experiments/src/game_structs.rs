dc45146eb68e84b619fc4e94c29eb2d3ef5c2959
jb <jacob.c.bonhomme@gmail.com>
2021-10-16 23:38:00 -0400

    Card System Revised. Ability to read cards in from file added.

--- card-experiments/src/game_structs.rs
+++ card-experiments/src/game_structs.rs
@@ -1,158 +1,168 @@
 //extern crate card_experiments;
 
 #[derive(Clone)]
-pub enum CardType{
-    Null, Heal, Defend, Attack,
-}
-
-#[derive(Clone)]
 pub struct Card <'a>{
     name: &'a str,
     desc: &'a str,
     cost: u32,
-    action: CardType,
-    value: u32,
+    action_list: Vec<u32>, //Actions represented by ID, ex: 0:Attack, 1:Defend, etc...
+    value_list: Vec<u32>, //Value of Actions, ie 1 could be 1 attack, 1 defend, etc...
 }
 
 impl <'a> Card <'a>{
-    pub fn new(name: &'a str, desc: &'a str, cost: u32, action: CardType, value: u32)->Card<'a>{
+    pub fn new(name: &'a str, desc: &'a str, cost: u32,action_list: Vec<u32>,value_list: Vec<u32>)->Card<'a>{
         Card{
-            name,desc,cost,action,value,
+            name,desc,cost,action_list,value_list,
         }
     }
 
-    pub fn getName(&self)->&str{
+    pub fn play_card(&self){
+        for (act,val) in self.action_list.iter().zip(self.value_list.iter()){
+            //calls to PLAY methods will go here
+            print!("{}, {}\n",act,val);
+        }
+    }
+
+    pub fn get_name(&self)->&str{
         self.name
     }
 
-    pub fn getDescription(&self)->&str{
+    pub fn get_description(&self)->&str{
         self.desc
     }
 
-    pub fn getCost(&self)->u32{
+    pub fn get_cost(&self)->u32{
         self.cost
     }
 
-    fn typeToString(&self)->String{
-        match self.action{
-            CardType::Heal => "Heal".to_string(),
-            CardType::Attack => "Attack".to_string(),
-            CardType::Defend => "Defend".to_string(),
-            CardType::Null => "Null".to_string(),
-        }
-    }
-
-    pub fn getType(&self)->String{
-        self.typeToString()
-    }
-
-    pub fn getValue(&self)->u32{
-        self.value
-    }
-
-    pub fn toString(&self)->String{
-        format!("{} {} {} {} {}",self.name,self.desc,self.cost,self.typeToString(),self.value)
+    pub fn to_string(&self)->String{
+        format!("{}: {} | {} energy.",self.name,self.desc,self.cost)
     }
 }
 
-pub struct Player <'a>{
-    full_health: u32, //may be redundant
+pub struct Player{
+    full_health: u32,
     curr_health: u32,
     full_energy: u32,
     curr_energy: u32,
-    hand_size: u32, //num of cards in player hand, may be removed
-    hand: Vec<Card<'a>>,
-    deck: Vec<Card<'a>>, //treat as queue
+    hand_size: usize, //num of cards in player hand, may be removed
+    hand: Vec<u32>, //Current held cards
+    deck: Vec<u32>, //Deck to draw from - treat as queue
+    discard: Vec<u32>, //Discarded deck
 }
 
-impl <'a> Player <'a>{ //HAND and DECK created as INTRINSIC VALUES
-    pub fn new(full_health: u32, curr_health: u32, full_energy: u32, curr_energy: u32, hand_size: u32)-> Player<'a>{
+impl Player{ //HAND and DECK created as INTRINSIC VALUES
+    pub fn new(full_health: u32, curr_health: u32, full_energy: u32, curr_energy: u32, hand_size: usize)-> Player{
         let hand = Vec::new();
         let deck = Vec::new();
-        Player{full_health,curr_health,full_energy,curr_energy,hand_size,hand,deck}
+        let discard = Vec::new();
+        Player{full_health,curr_health,full_energy,curr_energy,hand_size,hand,deck,discard}
     }
 
-    pub fn getFullHealth(&self)->u32{
+    pub fn get_full_health(&self)->u32{
         self.full_health
     }
 
-    pub fn getCurrHealth(&self)->u32{
+    pub fn get_curr_health(&self)->u32{
         self.curr_health
     }
 
-    pub fn getFullEnergy(&self)->u32{
+    pub fn get_full_energy(&self)->u32{
         self.full_energy
     }
 
-    pub fn getCurrEnergy(&self)->u32{
+    pub fn get_curr_energy(&self)->u32{
         self.curr_energy
     }
 
-    pub fn getHandSize(&self)->u32{
+    pub fn get_full_hand_size(&self)->usize{
         self.hand_size
     }
 
-    //pub fn getHand(&self)->&Vec<Card>{ Should we return the entire vec/vecdeque? Or abstract away?
-    //    &self.hand
-    //}
-
-    //pub fn getDeck(&self)->&VecDeque<Card>{
-    //    &self.deck
-    //}
-
-    pub fn setFullHealth(&mut self,h: u32){
+    pub fn set_full_health(&mut self,h: u32){
         self.full_health = h;
     }
 
-    pub fn setCurrHealth(&mut self,h:u32){
+    pub fn set_curr_health(&mut self,h:u32){
         self.curr_health = h;
     }
 
-    pub fn setFullEnergy(&mut self,h:u32){
+    pub fn set_full_energy(&mut self,h:u32){
         self.full_energy = h;
     }
 
-    pub fn setCurrEnergy(&mut self,h:u32){
+    pub fn set_curr_energy(&mut self,h:u32){
         self.curr_energy = h;
     }
 
-    pub fn setHandSize(&mut self, s:u32){
+    pub fn set_hand_size(&mut self, s:usize){
         self.hand_size = s;
     }
 
-    pub fn addCardToHand(&mut self,c: Card<'a>){
+    pub fn add_card_to_hand(&mut self,c: u32){ //add card to ACTIVE hand
         self.hand.push(c);
     }
 
-    pub fn addCardToDeck(&mut self,c: Card<'a>){
+    pub fn add_card_to_deck(&mut self,c: u32){ //add card to deck to DRAW from
         self.deck.push(c);
     }
 
-    pub fn deckSize(&self)->usize{
+    pub fn add_card_to_discard(&mut self,c:u32){ //add card to DISCARD PILE
+        self.discard.push(c);
+    }
+
+    pub fn get_deck_size(&self)->usize{
         self.deck.len()
     }
 
-    pub fn handSize(&self)->usize{
+    pub fn get_curr_hand_size(&self)->usize{
         self.hand.len()
     }
 
-    pub fn deckDelCard(&mut self){
-        self.deck.remove(0);
+    pub fn deck_del_card(&mut self){
+        if self.deck.len()>0{
+            self.deck.remove(0);
+        }
     }
 
-    pub fn handDelCard(&mut self,index:usize){
-        self.hand.remove(index);
+    pub fn hand_del_card(&mut self,index:usize){
+        if self.hand.len()>0{
+            self.hand.remove(index);
+        }
     }
 
-    pub fn drawCard(&self)->Card<'a>{
-        self.deck[0].clone()
+    pub fn hand_discard_card(&mut self,index:usize){ //Hand => Discard
+        if self.hand.len()>0{
+            self.add_card_to_discard(self.hand[index]);
+            self.hand.remove(index);
+        }
     }
 
-    pub fn selectHand(&self,index:usize)->Card<'a>{
-            self.hand[index].clone()
+    pub fn get_deck_card(&self)->Option<u32>{
+        if self.deck.len()>0{
+            Some(self.deck[0])
+        }else{
+            None
+        }
     }
 
-    pub fn toString(&self)->String{
+    pub fn draw_card(&mut self){ //Deck => Hand
+        if self.deck.len()>0 && self.hand.len()<self.hand_size{
+            self.add_card_to_hand(self.deck[0]);
+            self.deck_del_card();
+        }
+    }
+
+    pub fn select_hand(&self,index:usize)->Option<u32>{
+        if self.hand.len()>0{
+            Some(self.hand[index])
+        }else{
+            None
+        }
+
+    }
+
+    pub fn to_string(&self)->String{
         format!("{} {} {} {} {}",self.full_health,self.curr_health,self.full_energy,self.curr_energy,self.hand_size)
     }
 }
--- card-experiments/src/main.rs
+++ card-experiments/src/main.rs
@@ -3,34 +3,61 @@ mod game_structs;
 
 //TRY MAKING THE VECS IN PLAYER POINTERS SO THAT PLAYER NEVER HAS TO CHANGE ITSELF
 
-use game_structs::CardType;
 use game_structs::Card;
 use game_structs::Player;
+use std::fs;
+use std::collections::HashMap;
 //For card testing
 
-//fn drawCard<'a>(aPlayer: &'a mut Player)->Card<'a>{
-    //let tmp = aPlayer.drawCard();
-    //aPlayer.deckDelCard();
+//fn drawCard<'a>(a_player: &'a mut Player)->Card<'a>{
+    //let tmp = a_player.drawCard();
+    //a_player.deckDelCard();
     //tmp
 //}
 
-fn main(){
-    let aCard = Card::new("Test","This a test",3,CardType::Heal,3);
-    print!("{}\n",aCard.toString());
+pub fn populate_card_maprary<'a>(fileData: &'a str)->HashMap<u32,Card<'a>>{
+    let mut cards = HashMap::new();
+    for line in (fileData[4..]).split('\n'){ //Remove first character, \u was messing with things
+        if line.len()==0{ //If empty line, skip
+            continue;
+        }else if line.starts_with("##"){ //If commented line, skip
+            continue;
+        }
 
-    let mut aPlayer = Player::new(20,20,10,10,7);
-    print!("{}\n",aPlayer.toString());
+        let line_data: Vec<&str> = line.split("::").collect();
+        //Collect and parse data into new card
+        cards.insert(line_data[0].parse::<u32>().unwrap(),Card::new(line_data[1],line_data[2],line_data[3].parse::<u32>().unwrap(),line_data[4].split(',').map(|v| v.parse::<u32>().unwrap()).collect(),line_data[5].split(',').map(|v| v.parse::<u32>().unwrap()).collect()));
+    }
+    cards
+}
 
-    aPlayer.addCardToDeck(aCard);
+fn main(){ //File data read in then used to create library
+    let fileData = &fs::read_to_string("src/card-library.txt").expect("An error occurred whilst attempting to open the library.");
+    let card_map = populate_card_maprary(fileData);
 
-    //error handling here
+    let mut a_player = Player::new(20,20,10,10,7);
+    print!("{}\n",a_player.to_string());
 
-    aPlayer.addCardToHand(aPlayer.drawCard()); //
-    aPlayer.deckDelCard(); //cannot have a POP function, these 2 functions must serve as such.
+    for (id,card) in card_map.iter(){
+        a_player.add_card_to_deck(*id);
+    }
 
-    print!("{}\n",aPlayer.selectHand(0).toString());
+    for _i in 0..a_player.get_full_hand_size(){ //How hand would be populated
+        a_player.draw_card();
+    }
 
-    aPlayer.handDelCard(0);
+    for _i in 0..a_player.get_curr_hand_size(){
+        let c = a_player.select_hand(0);
+        if !c.is_none(){
+            let card = card_map.get(&c.unwrap()).unwrap(); //unwrap select hand, unwrap hash
+            print!("{}\n",card.to_string());
+            card.play_card();
+            a_player.hand_discard_card(0); //moves to discard deck in player struct
+        }
+
+    }
+
+    print!("Curr Deck Size: {}\nCurr Hand Size: {}\n",a_player.get_deck_size(),a_player.get_curr_hand_size())
 
 
 }
