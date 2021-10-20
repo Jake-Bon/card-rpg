7eaa36a48ddd70e9952ab932a2f13bcaf1a97560
jb <jacob.c.bonhomme@gmail.com>
2021-10-19 20:47:05 -0400

    CMD Output

--- card-experiments/src/main.rs
+++ card-experiments/src/main.rs
@@ -32,32 +32,45 @@ pub fn populate_card_maprary<'a>(fileData: &'a str)->HashMap<u32,Card<'a>>{
 }
 
 fn main(){ //File data read in then used to create library
+    print!("Reading in card library data...\n");
     let fileData = &fs::read_to_string("src/card-library.txt").expect("An error occurred whilst attempting to open the library.");
+    print!("File read successfully!\nBuilding card map...\n\n\n");
     let card_map = populate_card_maprary(fileData);
+    print!("Card map successfully built! Cards ready to be used!\n\n\n");
 
     let mut a_player = Player::new(20,20,10,10,7);
+    print!("Player object created.\n");
     print!("{}\n",a_player.to_string());
 
+    print!("\n\n\n");
     for (id,card) in card_map.iter(){
         a_player.add_card_to_deck(*id);
+        print!("{} added to deck!\n", card.get_name());
     }
+
+    print!("\n\n\n");
 
     for _i in 0..a_player.get_full_hand_size(){ //How hand would be populated
         a_player.draw_card();
+        print!("A card was transferred from DECK to HAND\n");
     }
+
+    print!("\n\n\n");
 
     for _i in 0..a_player.get_curr_hand_size(){
         let c = a_player.select_hand(0);
         if !c.is_none(){
+            print!("Card drawn from deck and played:\n");
             let card = card_map.get(&c.unwrap()).unwrap(); //unwrap select hand, unwrap hash
-            print!("{}\n",card.to_string());
+            print!("{}\nCard traits (used to play card):\n",card.to_string());
             card.play_card();
             a_player.hand_discard_card(0); //moves to discard deck in player struct
+            print!("\n");
         }
 
     }
 
-    print!("Curr Deck Size: {}\nCurr Hand Size: {}\n",a_player.get_deck_size(),a_player.get_curr_hand_size())
+    print!("Remaining number of cards in deck: {}\nRemaining cards in hand: {}\n",a_player.get_deck_size(),a_player.get_curr_hand_size())
 
 
 }
