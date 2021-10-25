5707dd2f356103321c17b86d46a1b70304ed7351
emifi <eaf53@pitt.edu>
2021-10-25 13:02:40 -0400

    .gitignore being a pain

--- target/debug/deps/cfg_if-5312f1413f00c09c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Emilio\Documents\GitHub\card-rpg\target\debug\deps\cfg_if-5312f1413f00c09c.rmeta: C:\Users\Emilio\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\Emilio\Documents\GitHub\card-rpg\target\debug\deps\libcfg_if-5312f1413f00c09c.rlib: C:\Users\Emilio\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\Emilio\Documents\GitHub\card-rpg\target\debug\deps\cfg_if-5312f1413f00c09c.d: C:\Users\Emilio\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\Emilio\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs:
