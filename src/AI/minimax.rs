0e3a39146be17676b83dcd11f22a41e42b8f4fb5
emifi <eaf53@pitt.edu>
2021-11-11 16:50:36 -0500

    Initial AI file structure

---
+++ src/AI/minimax.rs
@@ -0,0 +1,30 @@
+use crate::AI::ai_structs;
+
+/*
+// For reference: 
+// Minimax with alpha-beta pruning pseudocode pulled from https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/
+function minimax(node, depth, isMaximizingPlayer, alpha, beta):
+
+    if node is a leaf node :
+        return value of the node
+    
+    if isMaximizingPlayer :
+        bestVal = -INFINITY 
+        for each child node :
+            value = minimax(node, depth+1, false, alpha, beta)
+            bestVal = max( bestVal, value) 
+            alpha = max( alpha, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
+
+    else :
+        bestVal = +INFINITY 
+        for each child node :
+            value = minimax(node, depth+1, true, alpha, beta)
+            bestVal = min( bestVal, value) 
+            beta = min( beta, bestVal)
+            if beta <= alpha:
+                break
+        return bestVal
+*/
